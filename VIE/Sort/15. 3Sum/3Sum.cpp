f3b8ed48e9f77ad293523f587f6294f324307485
Phuc Nguyen <87957677+Nomatter2021@users.noreply.github.com>
2025-10-25 07:38:16 +0700

    Tối ưu lại source code
    Tinh chỉnh source code để có kết quả tốt hơn

--- VIE/Sort/15. 3Sum/3Sum.cpp
+++ VIE/Sort/15. 3Sum/3Sum.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-        std::ranges::sort(nums); //Sắp xếp mảng nums
+        std::sort(nums.begin(), nums.end()); // Sắp xếp mảng nums
         std::vector <std::vector <int>> rs; // Tạo vector trả kết quả
+        std::vector <int> sum; // Khởi tạo vector chứa kết quả tổng 3 số = 0
+        long long int T{0}; // Khởi tạo biến chứa kết quả tổng 3 số
         for(int i{0}; i < nums.size() - 2; ++i) // Tạo vòng lặp duyệt mảng
         {
             int l{i + 1}; // Đầu mút trái
@@ -19,23 +21,24 @@ public:
             {
                 while(l < r) // Vòng lặp di chuyển l và r để tìm tổng 0
                 {
-                    if(nums[i] + nums[l] + nums[r] < 0) // Di chuyển l nếu tổng < 0
+                    T = nums[i] + nums[l] + nums[r];
+                    if(T < 0) // Di chuyển l nếu tổng < 0
                     {
                         ++l;
                     }
-                    else if (nums[i] + nums[l] + nums[r] > 0) // Di chuyển l nếu tổng > 0
+                    else if (T > 0) // Di chuyển l nếu tổng > 0
                     {
                         --r;
                     }
                     else // Nhận kết quả
                     {
-                        std::vector <int> sum;
                         sum.push_back(nums[i]);
                         sum.push_back(nums[l]);
                         sum.push_back(nums[r]);
-                        rs.push_back(sum);
+                        rs.push_back(sum); // push back kết quả vào vector dùng để return kết quả
                         ++l;
                         --r;
+                        sum.clear(); // xóa dữ liệu cũ chuẩn bị cho vòng dữ liệu mới
                         while(l < r && nums[r] == nums[r + 1]) // Tìm giá trị bị trùng của r
                         {
                             --r;
@@ -51,4 +54,5 @@ public:
         };
         return rs; // Trả kết quả
     };
+
 };
