548e4ac7759ca711d39651011f0c6fa9f21e7a75
Phuc Nguyen <87957677+Nomatter2021@users.noreply.github.com>
2025-10-28 07:53:36 +0700

    Cập nhật file source code do add nhầm file

--- VIE/Hash Table/49. Group Anagrams/Group-Anagrams.cpp
+++ VIE/Hash Table/49. Group Anagrams/Group-Anagrams.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int n{static_cast<int>(nums.size()-1)}, i{n - 1}, j{n}; // Khởi tạo biến cần thiết
-        if(n == 1) // Ngắt sớm ở trường hợp đặc biệt chỉ có 2 số
-        {
-            std::reverse(nums.begin(), nums.end());
-        }
-        else
-        {
-            for(i; i >= 0; i--) // Khởi tạo vòng lặp i di chuyển từ phải qua trái để tìm cặp số mà ở đó số bên trái bé hơn số bên phải
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      std::vector<std::vector<std::string>> rs; // Tạo vector trả kết quả
+        std::unordered_map<std::string, std::vector<std::string>> HashT; // Tạo bảng băm
+        std::string key; // Tạo key
+        for(auto& i: strs) // Duyệt vector strs
             {
-                if(nums[i] < nums[i + 1])
-                {
-                    for(j; j > i; j--) // Khởi tạo vòng lặp j để tìm tiếp số đầu tiên trong các dãy số bên phải lớn hơn số vừa tìm được
-                    {
-                        if(nums[i] < nums[j])
-                        {
-                            std::swap(nums[i], nums[j]); // Đổi vị trí 2 số cho nhau
-                            break;
-                        };
-                    };
-                    std::reverse(nums.begin() + i + 1, nums.end()); / Đảo ngược toàn bộ dãy số từ vị trí i + 1
-                    break;
-                };
-                if(i == 0) // Điều kiện đặt biệt nếu i == 0 nghĩa là đây là hoán vị nhỏ nhất nên đảo ngược toàn bộ dãy số.
-                {
-                    std::reverse(nums.begin(), nums.end());
-                };
+                key = i;
+                std::sort(key.begin(), key.end()); // Băm key
+                HashT[key].emplace_back(i); // Nhóm kết quả
             };
-        };
+        for(auto& i : HashT) // Di chuyển kết quả từ bảng băm sang vector trả kết quả
+            {
+                rs.emplace_back(std::move(i.second));
+            };
+        return rs; // Trả kết quả
     };
 };
